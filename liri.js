4f7c6194925113f677b7342b88a5d5e50188f807
Ismael Correa <ismaelcorrea@Ismaels-MacBook-Pro.local>
2018-06-04 03:36:27 -0500

    Update

--- liri.js
+++ liri.js
@@ -1,23 +1,30 @@
 
 var dotenv = require("dotenv").config();
-var Twitter = require('twitter');
+var twitter = require('twitter');
+var spotify = require('node-spotify-api');
+var fs = require("fs");
 
-var client = new Twitter({
+
+var client = new twitter({
   consumer_key: 'V2Q9g1obLzwJvJgC5OzKDRoYp',
   consumer_secret: 'VQ4RovCW87sayeaypdeJJ4CiDnqSlKhvGPeVorPq8QbvaS1jpU',
   access_token_key: '303599661-RvgP2Hredac8NjmFGHIy0Z01kW2E8eeiY2S5C0oL',
   access_token_secret: 'voppSkFvEyzcvdKWjoBmpHlEs4nz1UB0VPJXuYSBYwpdq'
 });
 
+var spotify = new spotify({
+  id: "7871516a8b57449dab864f186ffc070b",
+  secret: "3f10c217d93e4f4b85090f47e8581152"
+});
+
 var nuevoControl = process.argv
 var nodeArguments = nuevoControl[2];
-var spotifySong = nuevoControl[3];
+var thirdArgument = nuevoControl[3];
 
 
 //my-tweets conditional
-if (twitter === "my-tweets"){
+if (nodeArguments === "my-tweets"){
   function tweeter(){
-    console.log("Yo whats good my nukkah. We oout here nukkah. post up and szend them dfucking shots. nukkah.");
     var tweets = { screen_name: 'nodejs', count: 5 }
     client.get('statuses/user_timeline',function(error, tweets, response) {
       console.log(tweets);
@@ -26,14 +33,57 @@ if (twitter === "my-tweets"){
     tweeter();
   }
 
-
-  var song = ""
-
   //spotify-this-song conditional
   if (nodeArguments === "spotify-this-song"){
-    function spotty(){
-      
+    function spotty(thirdArgument){
+      if(!thirdArgument){
+        spotifySong = "The Sign"
+      }
+      var artist = {type:"artist",query:thirdArgument};
+      spotify.search(artist,function(err,data){
+        if(err){
+          return console.log("Error occured:"+ err);
+        }
+        console.log(data);
+      });
+      var songName = {type:"track",query:thirdArgument}
+      spotify.search(songName,function(err,data){
+        if(err){
+          return console.log("Error occured:" + err);
+        }
+        console.log(data);
+      })
+      var  album = {type:"album",query:thirdArgument}
+      spotify.search(album,function(err,data){
+        if(err){
+          return console.log("Error occured:" + err);
+        }
+        console.log(data);
+      })
     }
+    spotty();
   }
 
+  if (nodeArguments === "movie-this"){
+    var url = "http://www.omdbapi.com/?apikey=trilogy&" + thirdArgument;
+    console.log(url);
 
+    //the OMDB API url should be displaying all relevant information through retreiving the information
+
+  }
+
+  var content = "random.txt"
+
+  //do-what-says
+  if(nodeArguments === "do-what-it-says"){
+    
+    function readFile(){
+      var tex = fs.readFile(content,"utf8",function(err,data){
+        console.log(data);
+      });
+      }
+      //this component should take in the output from the readFile and input it in the spotty function
+      readFile();
+      spotty();
+      
+    }
